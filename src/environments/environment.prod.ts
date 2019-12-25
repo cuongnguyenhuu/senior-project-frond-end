9571ccbde6c836d0ecec7b7a94c372a9bdd4e52f
cuongnguyenhuu <35827811+cuongnguyenhuu@users.noreply.github.com>
2019-12-25 11:25:24 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBAz4JCWnYZPP5vs_ZwMr0kawKVi58zuQ8",
+    authDomain: "senior-project-1568085904273.firebaseapp.com",
+    databaseURL: "https://senior-project-1568085904273.firebaseio.com",
+    projectId: "senior-project-1568085904273",
+    storageBucket: "senior-project-1568085904273.appspot.com",
+    messagingSenderId: "817163355907",
+    appId: "1:817163355907:web:51e4557f183e30cf4acb36",
+    measurementId: "G-JB63JHRJRV"
+  }
 };
