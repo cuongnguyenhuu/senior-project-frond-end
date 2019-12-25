0decf98fc0602b011125c04732721157cb88e970
Cuong (currie) H.NGUYEN <currie@enclave.vn>
2019-12-25 13:22:22 +0700

    change server URL

--- src/app/models/API.ts
+++ src/app/models/API.ts
@@ -1,6 +1,6 @@
 export class API{
 
-    link = "http://ec2-13-229-134-154.ap-southeast-1.compute.amazonaws.com:8080"
+    link = "http://ec2-3-1-101-0.ap-southeast-1.compute.amazonaws.com:8080"
 
     public getLink(){
         return this.link;
